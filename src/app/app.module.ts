b3a47d39b20cf4516e2b1966bd76aee50f6ed607
AzureAD\AndreaFiori <andrea.fiori@surgiq.com>
2021-12-17 15:30:06 +0100

    Removed material, added icons and calendar header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MatGridListModule } from '@angular/material/grid-list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { MatTabsModule } from '@angular/material';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
@@ -12,17 +10,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { CalendarDemoComponent } from './calendar-demo/calendar-demo.component';
 
 import { AppComponent } from './app.component';
+import { CalendarHeaderComponent } from './calendar-header/calendar-header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    CalendarDemoComponent
+    CalendarDemoComponent,
+    CalendarHeaderComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatGridListModule,
-    MatTabsModule,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
     AppRoutingModule,
   ],
